82599d9443bf4b3fc8b8dcd36ea531d0eab7277e
Djay <69097682+Djay47@users.noreply.github.com>
2022-12-05 08:16:55 +0700

    Add files via upload

---
+++ project-restoran-ci4_changelog/revisi00-video1-10/app/Controllers/Front/HomePage.php
@@ -0,0 +1,15 @@
+<?php namespace App\Controllers\Front;
+
+use App\Controllers\BaseController;
+
+class HomePage extends BaseController
+{
+	public function index()
+	{
+		// return view('welcome_message');
+		echo "<h1>Hello Wordl!</h1>";
+	}
+
+	//--------------------------------------------------------------------
+
+}
