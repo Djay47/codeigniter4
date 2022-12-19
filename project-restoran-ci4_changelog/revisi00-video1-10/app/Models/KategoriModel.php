49febe73e9d3f426941dce468c1a005a9674c288
Djay <69097682+Djay47@users.noreply.github.com>
2022-12-19 11:00:41 +0700

    Delete project-restoran-ci4_changelog directory

--- project-restoran-ci4_changelog/revisi00-video1-10/app/Models/KategoriModel.php
+++
@@ -1,9 +0,0 @@
-<?php namespace APP\Models;
-
-use CodeIgniter\Model;
-
-class KategoriModel extends Model
-{
-	protected $table = "kategori";
-	protected $allowedFields = ['kategori', 'keterangan'];
-}
--- project-restoran-ci4_changelog/revisi00-video1-10/app/Views/kategori/update.php
+++
@@ -1,7 +0,0 @@
-<?php $this->extend('layout/template') ?>
-
-<?php $this->section('content'); ?>
-
-<h1>Update Data Form</h1>
-
-<?php $this->endSection(); ?>
